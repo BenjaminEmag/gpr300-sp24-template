6f81f639d45fc0a270a2f5234172c14eca54653b
BenjaminEmag <benjaminemag@gmail.com>
2025-01-27 18:37:37 -0500

    Add Camera and Spinning Monkey

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -8,6 +8,13 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/transform.h>
+
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
 void drawUI();
@@ -18,9 +25,26 @@ int screenHeight = 720;
 float prevFrameTime;
 float deltaTime;
 
+ew::Camera camera;
+ew::CameraController cameraController;
+
 int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK); //Back face culling
+	glEnable(GL_DEPTH_TEST); //Depth testing
+
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
+	ew::Transform monkeyTransform;
+
+
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //Look at the center of the scene
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f; //Vertical field of view, in degrees
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -29,9 +53,25 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
+
+		// Spin Monkey
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+		shader.setMat4("_Model", monkeyTransform.modelMatrix());
+
+
+		// Move Camera
+		cameraController.move(window, &camera, deltaTime);
+
 		//RENDER
-		glClearColor(0.6f,0.8f,0.92f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+
+		shader.use();
+
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+
+		monkeyModel.draw(); //Draws monkey model using current shader
 
 		drawUI();
 
@@ -40,13 +80,21 @@ int main() {
 	printf("Shutting down...");
 }
 
+void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
+
+
 void drawUI() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
-	ImGui::Begin("Settings");
-	ImGui::Text("Add Controls Here!");
+	ImGui::Begin("Cool Monkey");
+	if (ImGui::Button("Reset Camera")) resetCamera(&camera, &cameraController);
+	
 	ImGui::End();
 
 	ImGui::Render();
@@ -58,6 +106,7 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 	screenWidth = width;
 	screenHeight = height;
+	camera.aspectRatio = (float)width / height;
 }
 
 /// <summary>
